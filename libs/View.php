69e63526959e877276a233ef6042606c71ebbfdc
PRESERCOMP <desarrollo@presercomp.cl>
2020-06-23 17:16:05 -0400

    Cambios clase 23 de junio

--- libs/View.php
+++ libs/View.php
@@ -8,6 +8,7 @@ class View {
     public function render($view, $name, $panel=false) {
         $this->view = $view;
         require TEMPLATE . "/header.php";
+        require TEMPLATE . "/menu.php";
         require 'views/' . $view. '/'. $name . '.php';
         require TEMPLATE . "/footer.php";
     }

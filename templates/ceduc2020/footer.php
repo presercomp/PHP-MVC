9b5393b8adbfd0fe296f54b720d1d0739246dd54
PRESERCOMP <desarrollo@presercomp.cl>
2020-06-30 16:43:56 -0400

    Modificación de estructura visual

--- templates/ceduc2020/footer.php
+++ templates/ceduc2020/footer.php
@@ -18,5 +18,37 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+    $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- ChartJS -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/chart.js/Chart.min.js"></script>
+    <!-- Sparkline -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/sparklines/sparkline.js"></script>
+    <!-- JQVMap -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="<?php echo TEMPLATE; ?>/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <!-- daterangepicker -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/moment/moment.min.js"></script>
+    <script src="<?php echo TEMPLATE; ?>/plugins/daterangepicker/daterangepicker.js"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- Summernote -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/summernote/summernote-bs4.min.js"></script>
+    <!-- overlayScrollbars -->
+    <script src="<?php echo TEMPLATE; ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo TEMPLATE; ?>/js/adminlte.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="<?php echo TEMPLATE; ?>/js/pages/dashboard.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?php echo TEMPLATE; ?>/js/demo.js"></script>
     </body>
 </html>
--- templates/ceduc2020/header.php
+++ templates/ceduc2020/header.php
@@ -11,8 +11,12 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="<?php echo TEMPLATE; ?>/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo TEMPLATE; ?>/css/adminlte.min.css">
+
+    <link rel="stylesheet" href="<?php echo TEMPLATE; ?>/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
